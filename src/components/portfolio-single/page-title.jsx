7f69dab4b50388b9dcf4755da03da24354d2ca2d
Dru-Go <drumac2@gmail.com>
2020-06-11 18:14:02 +0300

    Added portfolio-single section of the page

---
+++ src/components/portfolio-single/page-title.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function pageTitle() {
+  return (
+    <section class="page-title centred">
+      <div class="container">
+        <div class="content-box">
+          <h1>Portfolio Single</h1>
+          <ul class="bread-crumb clearfix">
+            <li>
+              <a href="index.html">Home</a>
+            </li>
+            <li>Portfolio Single 02</li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/components/portfolio-single/portfolio-section.jsx
@@ -0,0 +1,164 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+
+export default function portfolioSection() {
+  return (
+    <section class="portfolio-single-2 portfolio-single">
+      <div class="container">
+        <div class="inner-box">
+          <div class="title-box clearfix">
+            <div class="sec-title pull-left">
+              <h1>
+                Exercitation ullam
+                <br />
+                laboris aliquip ex commodo.
+              </h1>
+            </div>
+            <ul class="social-list social-share pull-right">
+              <li>
+                <h4>Share:</h4>
+              </li>
+              <li>
+                <a href="#">
+                  <i class="fab fa-facebook-f"></i>
+                </a>
+                <a href="#">
+                  <i class="fab fa-twitter"></i>
+                </a>
+                <a href="#">
+                  <i class="fab fa-vimeo-v"></i>
+                </a>
+                <a href="#">
+                  <i class="fab fa-linkedin-in"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div class="image-column">
+            <div class="row">
+              <div class="col-lg-6 col-md-12 col-sm-12 big-column">
+                <div class="image-box">
+                  <figure class="image">
+                    <a
+                      href="images/portfolio/portfolio-single-2.jpg"
+                      class="lightbox-image"
+                      data-fancybox="gallery"
+                    >
+                      <img
+                        src="images/portfolio/portfolio-single-2.jpg"
+                        alt=""
+                      />
+                    </a>
+                  </figure>
+                </div>
+              </div>
+              <div class="col-lg-6 col-md-12 col-sm-12 big-column">
+                <div class="row">
+                  <div class="col-lg-12 col-md-12 col-sm-12 small-column">
+                    <div class="image-box">
+                      <figure class="image">
+                        <a
+                          href="images/portfolio/portfolio-single-3.jpg"
+                          class="lightbox-image"
+                          data-fancybox="gallery"
+                        >
+                          <img
+                            src="images/portfolio/portfolio-single-3.jpg"
+                            alt=""
+                          />
+                        </a>
+                      </figure>
+                    </div>
+                  </div>
+                  <div class="col-lg-6 col-md-6 col-sm-12 small-column">
+                    <div class="image-box">
+                      <figure class="image">
+                        <a
+                          href="images/portfolio/portfolio-single-4.jpg"
+                          class="lightbox-image"
+                          data-fancybox="gallery"
+                        >
+                          <img
+                            src="images/portfolio/portfolio-single-4.jpg"
+                            alt=""
+                          />
+                        </a>
+                      </figure>
+                    </div>
+                  </div>
+                  <div class="col-lg-6 col-md-6 col-sm-12 small-column">
+                    <div class="image-box">
+                      <figure class="image">
+                        <a
+                          href="images/portfolio/portfolio-single-5.jpg"
+                          class="lightbox-image"
+                          data-fancybox="gallery"
+                        >
+                          <img
+                            src="images/portfolio/portfolio-single-5.jpg"
+                            alt=""
+                          />
+                        </a>
+                      </figure>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="info-column">
+            <ul class="info-list clearfix">
+              <li>
+                <h4>Date</h4>
+                May 20, 2019
+              </li>
+              <li>
+                <h4>Clients</h4>
+                Jack Nicholson
+              </li>
+              <li>
+                <h4>Type</h4>
+                Work Space Design
+              </li>
+            </ul>
+          </div>
+          <div class="lower-content">
+            <h2>Innovative solutions</h2>
+            <div class="text">
+              <p>
+                Dolor sit amet consectetur elit sed do eiusmod tempor incididunt
+                labore dolore magna aliqua enim minim veniam quis nostrud
+                exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat.duis aute irure dolor in reprehenderit. voluptate
+                velit esse cillum dolore fugiat nulla pariatur Excepteur sint
+                occaecat cupidatat non proident sunt in culpaqui officia
+                deserunt mollit anim id est laborum sed perspiciatis unde omnis
+                natus error sit voluptatem accusantium doloremque laudantium,
+                totam rem aperiam eaque ipsa quae ab illo inventore.
+              </p>
+            </div>
+            <ul class="list-item">
+              <li>Various Analysis Options</li>
+              <li>Page Load Details (time, size, number of requests)</li>
+              <li>Waterfall, Video and Report History</li>
+            </ul>
+            <div class="load-more-option">
+              <div class="clearfix">
+                <div class="prev-btn pull-left">
+                  <a href="#">
+                    <i class="fa fa-arrow-left"></i>Previous
+                  </a>
+                </div>
+                <div class="next-btn pull-right">
+                  <a href="#">
+                    Next<i class="fa fa-arrow-right"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
