9c8c17342a7b9dddfbfbb214e4cc4f38b1359a12
Dru-Go <drumac2@gmail.com>
2020-06-11 18:28:06 +0300

    Added the blog-classic section of the page

---
+++ src/components/blog-classic/blog-section.jsx
@@ -0,0 +1,211 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+
+export default function blogSection() {
+  return (
+    <section class="blog-classic sidebar-page-container">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 col-md-12 col-sm-12 offset-lg-2 content-side">
+            <div class="blog-classic-content">
+              <div
+                class="news-block-two wow fadeInUp"
+                data-wow-delay="00ms"
+                data-wow-duration="1500ms"
+              >
+                <div class="inner-box">
+                  <figure class="image-box">
+                    <a href="blog-single.html">
+                      <img src="images/resource/news-11.jpg" alt="" />
+                    </a>
+                  </figure>
+                  <div class="lower-content">
+                    <div class="post-date">April 27, 2019</div>
+                    <h2>
+                      <a href="blog-single.html">
+                        Startups Are Still Out for Blood
+                      </a>
+                    </h2>
+                    <div class="text">
+                      <p>
+                        Lorem ipsum dolor amet consectetur adipisicing sed do
+                        eiusmod tempor incididunt ut labore magna aliqua. enim
+                        ad minim veniam quis nostrud exercitation ullamco
+                        laboris nisi ut aliquip ex ea commodo consequat. Duis
+                        aute irure dolor in reprehenderit.
+                      </p>
+                    </div>
+                    <div class="btn-box">
+                      <a href="blog-single.html" class="btn-style-two">
+                        Read More
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div
+                class="news-block-two wow fadeInUp"
+                data-wow-delay="00ms"
+                data-wow-duration="1500ms"
+              >
+                <div class="inner-box">
+                  <figure class="image-box">
+                    <a href="blog-single.html">
+                      <img src="images/resource/news-12.jpg" alt="" />
+                    </a>
+                  </figure>
+                  <div class="lower-content">
+                    <div class="post-date">April 25, 2019</div>
+                    <h2>
+                      <a href="blog-single.html">
+                        Designing for Appropriate Interaction
+                      </a>
+                    </h2>
+                    <div class="text">
+                      <p>
+                        Lorem ipsum dolor amet consectetur adipisicing sed do
+                        eiusmod tempor incididunt ut labore magna aliqua. enim
+                        ad minim veniam quis nostrud exercitation ullamco
+                        laboris nisi ut aliquip ex ea commodo consequat. Duis
+                        aute irure dolor in reprehenderit.
+                      </p>
+                    </div>
+                    <div class="btn-box">
+                      <a href="blog-single.html" class="btn-style-two">
+                        Read More
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div
+                class="news-block-two wow fadeInUp"
+                data-wow-delay="00ms"
+                data-wow-duration="1500ms"
+              >
+                <div class="inner-box">
+                  <div class="lower-content">
+                    <div class="post-date">April 24, 2019</div>
+                    <h2>
+                      <a href="blog-single.html">
+                        The Dark Side of Fitness Tracking
+                      </a>
+                    </h2>
+                    <div class="text">
+                      <p>
+                        Lorem ipsum dolor amet consectetur adipisicing sed do
+                        eiusmod tempor incididunt ut labore magna aliqua.enim ad
+                        minim veniam quis nostrud exercitation ullamco laboris
+                        nisi ut aliquip commodo consequat. Duis aute irure dolor
+                        reprehen.
+                      </p>
+                    </div>
+                    <div class="btn-box">
+                      <a href="blog-single.html" class="btn-style-two">
+                        Read More
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div
+                class="news-block-two wow fadeInUp"
+                data-wow-delay="00ms"
+                data-wow-duration="1500ms"
+              >
+                <div class="inner-box">
+                  <figure class="image-box">
+                    <a href="blog-single.html">
+                      <img src="images/resource/news-13.jpg" alt="" />
+                    </a>
+                  </figure>
+                  <div class="lower-content">
+                    <div class="post-date">April 23, 2019</div>
+                    <h2>
+                      <a href="blog-single.html">
+                        Designing for Appropriate Interaction
+                      </a>
+                    </h2>
+                    <div class="text">
+                      <p>
+                        Lorem ipsum dolor amet consectetur adipisicing sed do
+                        eiusmod tempor incididunt ut labore magna aliqua. enim
+                        ad minim veniam quis nostrud exercitation ullamco
+                        laboris nisi ut aliquip ex ea commodo consequat. Duis
+                        aute irure dolor in reprehenderit.
+                      </p>
+                    </div>
+                    <div class="btn-box">
+                      <a href="blog-single.html" class="btn-style-two">
+                        Read More
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div
+                class="news-block-two wow fadeInUp"
+                data-wow-delay="00ms"
+                data-wow-duration="1500ms"
+              >
+                <div class="inner-box">
+                  <figure class="image-box">
+                    <a href="blog-single.html">
+                      <img src="images/resource/news-14.jpg" alt="" />
+                    </a>
+                  </figure>
+                  <div class="lower-content">
+                    <div class="post-date">April 20, 2019</div>
+                    <h2>
+                      <a href="blog-single.html">
+                        Startups Are Still Out for Blood
+                      </a>
+                    </h2>
+                    <div class="text">
+                      <p>
+                        Lorem ipsum dolor amet consectetur adipisicing sed do
+                        eiusmod tempor incididunt ut labore magna aliqua. enim
+                        ad minim veniam quis nostrud exercitation ullamco
+                        laboris nisi ut aliquip ex ea commodo consequat. Duis
+                        aute irure dolor in reprehenderit.
+                      </p>
+                    </div>
+                    <div class="btn-box">
+                      <a href="blog-single.html" class="btn-style-two">
+                        Read More
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="pagination-wrapper clearfix">
+                <ul class="pagination centred">
+                  <li>
+                    <a href="#">Prev</a>
+                  </li>
+                  <li>
+                    <a href="#" class="active">
+                      1
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">2</a>
+                  </li>
+                  <li>
+                    <a href="#">3</a>
+                  </li>
+                  <li>
+                    <a href="#">4</a>
+                  </li>
+                  <li>
+                    <a href="#">Next</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
