301652ef03d5273b78d394581a4d8a24fec6003e
Dru-Go <drumac2@gmail.com>
2020-06-11 18:05:24 +0300

    Added th portfolio section of the page

---
+++ src/components/portfolio/portfolio-section.jsx
@@ -0,0 +1,374 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+
+export default function portfolioSection() {
+  return (
+    <section class="portfolio-page-2 portfolio-page">
+      <div class="container-fluid">
+        <div class="sortable-masonry">
+          <div class="filters">
+            <ul class="filter-tabs filter-btns centred clearfix">
+              <li class="active filter" data-role="button" data-filter=".all">
+                All
+              </li>
+              <li class="filter" data-role="button" data-filter=".design">
+                Design
+              </li>
+              <li
+                class="filter"
+                data-role="button"
+                data-filter=".web_development "
+              >
+                Web Development
+              </li>
+              <li class="filter" data-role="button" data-filter=".illustration">
+                Illustration
+              </li>
+              <li class="filter" data-role="button" data-filter=".photograpy ">
+                Photograpy
+              </li>
+            </ul>
+          </div>
+          <div class="items-container row clearfix">
+            <div class="col-lg-4 col-md-6 col-sm-12 masonry-item small-column all illustration design web_development">
+              <div class="portfolio-block-two">
+                <div class="inner-box">
+                  <figure class="image-box">
+                    <img src="images/portfolio/portfolio-14.jpg" alt="" />
+                  </figure>
+                  <div class="content-box">
+                    <ul class="link-box">
+                      <li>
+                        <a
+                          href="images/portfolio/portfolio-14.jpg"
+                          class="lightbox-image"
+                          data-fancybox="gallery"
+                        >
+                          <i class="fa fa-plus"></i>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="portfolio-single.html">
+                          <i class="fa fa-link"></i>
+                        </a>
+                      </li>
+                    </ul>
+                    <div class="content">
+                      <h3>
+                        <a href="portfolio-single.html">Virtual Integration</a>
+                      </h3>
+                      <div class="text">
+                        Lorem ipsum dolor sit amet consectetur adipicing elit
+                        sed do usmod tempor incididunt enim minim veniam quis
+                        nostrud.
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-12 masonry-item small-column all design photograpy">
+              <div class="portfolio-block-two">
+                <div class="inner-box">
+                  <figure class="image-box">
+                    <img src="images/portfolio/portfolio-15.jpg" alt="" />
+                  </figure>
+                  <div class="content-box">
+                    <ul class="link-box">
+                      <li>
+                        <a
+                          href="images/portfolio/portfolio-15.jpg"
+                          class="lightbox-image"
+                          data-fancybox="gallery"
+                        >
+                          <i class="fa fa-plus"></i>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="portfolio-single.html">
+                          <i class="fa fa-link"></i>
+                        </a>
+                      </li>
+                    </ul>
+                    <div class="content">
+                      <h3>
+                        <a href="portfolio-single.html">Virtual Integration</a>
+                      </h3>
+                      <div class="text">
+                        Lorem ipsum dolor sit amet consectetur adipicing elit
+                        sed do usmod tempor incididunt enim minim veniam quis
+                        nostrud.
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-12 masonry-item small-column all illustration web_development">
+              <div class="portfolio-block-two">
+                <div class="inner-box">
+                  <figure class="image-box">
+                    <img src="images/portfolio/portfolio-16.jpg" alt="" />
+                  </figure>
+                  <div class="content-box">
+                    <ul class="link-box">
+                      <li>
+                        <a
+                          href="images/portfolio/portfolio-16.jpg"
+                          class="lightbox-image"
+                          data-fancybox="gallery"
+                        >
+                          <i class="fa fa-plus"></i>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="portfolio-single.html">
+                          <i class="fa fa-link"></i>
+                        </a>
+                      </li>
+                    </ul>
+                    <div class="content">
+                      <h3>
+                        <a href="portfolio-single.html">Virtual Integration</a>
+                      </h3>
+                      <div class="text">
+                        Lorem ipsum dolor sit amet consectetur adipicing elit
+                        sed do usmod tempor incididunt enim minim veniam quis
+                        nostrud.
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-12 masonry-item small-column all branding photograpy">
+              <div class="portfolio-block-two">
+                <div class="inner-box">
+                  <figure class="image-box">
+                    <img src="images/portfolio/portfolio-17.jpg" alt="" />
+                  </figure>
+                  <div class="content-box">
+                    <ul class="link-box">
+                      <li>
+                        <a
+                          href="images/portfolio/portfolio-17.jpg"
+                          class="lightbox-image"
+                          data-fancybox="gallery"
+                        >
+                          <i class="fa fa-plus"></i>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="portfolio-single.html">
+                          <i class="fa fa-link"></i>
+                        </a>
+                      </li>
+                    </ul>
+                    <div class="content">
+                      <h3>
+                        <a href="portfolio-single.html">Virtual Integration</a>
+                      </h3>
+                      <div class="text">
+                        Lorem ipsum dolor sit amet consectetur adipicing elit
+                        sed do usmod tempor incididunt enim minim veniam quis
+                        nostrud.
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-12 masonry-item small-column all illustration design web_development">
+              <div class="portfolio-block-two">
+                <div class="inner-box">
+                  <figure class="image-box">
+                    <img src="images/portfolio/portfolio-18.jpg" alt="" />
+                  </figure>
+                  <div class="content-box">
+                    <ul class="link-box">
+                      <li>
+                        <a
+                          href="images/portfolio/portfolio-18.jpg"
+                          class="lightbox-image"
+                          data-fancybox="gallery"
+                        >
+                          <i class="fa fa-plus"></i>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="portfolio-single.html">
+                          <i class="fa fa-link"></i>
+                        </a>
+                      </li>
+                    </ul>
+                    <div class="content">
+                      <h3>
+                        <a href="portfolio-single.html">Virtual Integration</a>
+                      </h3>
+                      <div class="text">
+                        Lorem ipsum dolor sit amet consectetur adipicing elit
+                        sed do usmod tempor incididunt enim minim veniam quis
+                        nostrud.
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-12 masonry-item small-column all design illustration photograpy">
+              <div class="portfolio-block-two">
+                <div class="inner-box">
+                  <figure class="image-box">
+                    <img src="images/portfolio/portfolio-19.jpg" alt="" />
+                  </figure>
+                  <div class="content-box">
+                    <ul class="link-box">
+                      <li>
+                        <a
+                          href="images/portfolio/portfolio-19.jpg"
+                          class="lightbox-image"
+                          data-fancybox="gallery"
+                        >
+                          <i class="fa fa-plus"></i>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="portfolio-single.html">
+                          <i class="fa fa-link"></i>
+                        </a>
+                      </li>
+                    </ul>
+                    <div class="content">
+                      <h3>
+                        <a href="portfolio-single.html">Virtual Integration</a>
+                      </h3>
+                      <div class="text">
+                        Lorem ipsum dolor sit amet consectetur adipicing elit
+                        sed do usmod tempor incididunt enim minim veniam quis
+                        nostrud.
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-12 masonry-item small-column all illustration design web_development">
+              <div class="portfolio-block-two">
+                <div class="inner-box">
+                  <figure class="image-box">
+                    <img src="images/portfolio/portfolio-20.jpg" alt="" />
+                  </figure>
+                  <div class="content-box">
+                    <ul class="link-box">
+                      <li>
+                        <a
+                          href="images/portfolio/portfolio-20.jpg"
+                          class="lightbox-image"
+                          data-fancybox="gallery"
+                        >
+                          <i class="fa fa-plus"></i>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="portfolio-single.html">
+                          <i class="fa fa-link"></i>
+                        </a>
+                      </li>
+                    </ul>
+                    <div class="content">
+                      <h3>
+                        <a href="portfolio-single.html">Virtual Integration</a>
+                      </h3>
+                      <div class="text">
+                        Lorem ipsum dolor sit amet consectetur adipicing elit
+                        sed do usmod tempor incididunt enim minim veniam quis
+                        nostrud.
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-12 masonry-item small-column all design photograpy">
+              <div class="portfolio-block-two">
+                <div class="inner-box">
+                  <figure class="image-box">
+                    <img src="images/portfolio/portfolio-21.jpg" alt="" />
+                  </figure>
+                  <div class="content-box">
+                    <ul class="link-box">
+                      <li>
+                        <a
+                          href="images/portfolio/portfolio-21.jpg"
+                          class="lightbox-image"
+                          data-fancybox="gallery"
+                        >
+                          <i class="fa fa-plus"></i>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="portfolio-single.html">
+                          <i class="fa fa-link"></i>
+                        </a>
+                      </li>
+                    </ul>
+                    <div class="content">
+                      <h3>
+                        <a href="portfolio-single.html">Virtual Integration</a>
+                      </h3>
+                      <div class="text">
+                        Lorem ipsum dolor sit amet consectetur adipicing elit
+                        sed do usmod tempor incididunt enim minim veniam quis
+                        nostrud.
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-12 masonry-item small-column all illustration web_development">
+              <div class="portfolio-block-two">
+                <div class="inner-box">
+                  <figure class="image-box">
+                    <img src="images/portfolio/portfolio-22.jpg" alt="" />
+                  </figure>
+                  <div class="content-box">
+                    <ul class="link-box">
+                      <li>
+                        <a
+                          href="images/portfolio/portfolio-22.jpg"
+                          class="lightbox-image"
+                          data-fancybox="gallery"
+                        >
+                          <i class="fa fa-plus"></i>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="portfolio-single.html">
+                          <i class="fa fa-link"></i>
+                        </a>
+                      </li>
+                    </ul>
+                    <div class="content">
+                      <h3>
+                        <a href="portfolio-single.html">Virtual Integration</a>
+                      </h3>
+                      <div class="text">
+                        Lorem ipsum dolor sit amet consectetur adipicing elit
+                        sed do usmod tempor incididunt enim minim veniam quis
+                        nostrud.
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="load-btn centred">
+          <a href="#">Load More</a>
+        </div>
+      </div>
+    </section>
+  );
+}
